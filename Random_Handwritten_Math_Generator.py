9c7a00438378e55b12d8ff32abafd19dce904623
robin <robin.xw@hotmail.com>
2018-02-02 18:36:06 -0800

    update

--- Random_Handwritten_Math_Generator.py
+++ Random_Handwritten_Math_Generator.py
@@ -1,4 +1,4 @@
-import cv2, os, random, argparse, logging, sys
+import cv2, os, random, argparse, logging, sys, glob
 from PIL import Image
 import numpy as np
 from datetime import datetime
@@ -13,26 +13,35 @@ def process_args(args):
     parser.add_argument('--output-path_txt', dest='output_path_txt',
                         type=str, default='default',
                         help=('Output text file path, note: default set to same as image output path'))
-    parser.add_argument('--pad-size', dest='pad_size',
-                        type=int, default=8,
-                        help=('Pad the image to the top, left, bottom, right with whitespace of size Default=8.'))
+    parser.add_argument('--random-pad-size', dest='random_pad_size',
+                        type=list, default=range(12,45),
+                        help=('Pad the image to the top, left, bottom, right with whitespace of size random.'))
+    parser.add_argument('--backgrounded', dest='backgrounded',
+                        type=bool, default=False,
+                        help=('random pick a background, default white'))
+    parser.add_argument('--background-path', dest='background_path',
+                        type=str, default="./backgrounds/",
+                        help=('background image file path'))
     parser.add_argument('--batch-size', dest='batch_size',
                         type=int, default=1,
                         help=('Generate a batch of random images'))
+    parser.add_argument('--random_text_size', dest='random_text_size',
+                        type=list, default=range(80,150),
+                        help=('add randomness to text size'))
     
     parameters = parser.parse_args(args)
     return parameters
 
-def img_resize_open(imagepath, lst, lst_path):
-    baseheight = 60
+def img_resize_open(imagepath, lst, lst_path, standard_height=100):
+    baseheight = standard_height
     if lst[lst_path.index(imagepath)] in operation_lst+symbol:
-        baseheight = 45
+        baseheight = int(standard_height*0.8)
     if lst[lst_path.index(imagepath)] == '-':
-        baseheight = 6
+        baseheight = int(standard_height*0.15)
     if lst[lst_path.index(imagepath)] == '.':
-        baseheight = 10
+        baseheight = int(standard_height*0.15)
     if lst[lst_path.index(imagepath)] == '=':
-        baseheight = 18
+        baseheight = int(standard_height*0.4)
     img = Image.open(imagepath)
     hpercent = (baseheight/float(img.size[1]))
     wsize = int((float(img.size[0])*float(hpercent)))
@@ -48,13 +57,17 @@ def main(args):
     parameters = process_args(args)
     output_path = parameters.output_path
     output_path_txt = parameters.output_path_txt
-    pad_size = parameters.pad_size
+    random_pad_size = parameters.random_pad_size
+    backgrounded = parameters.backgrounded
+    background_path = parameters.background_path
     batch_size = parameters.batch_size
+    random_text_size = parameters.random_text_size
     
     if output_path_txt == 'default':
         output_path_txt = output_path
     
     for batch in range(0, batch_size):
+        pad_size = random.choice(random_pad_size)
         lst = []
         lst_path = []
         equation = [str(round(random.uniform(0, 99),1))
@@ -75,16 +88,19 @@ def main(args):
             img = random.choice([x for x in os.listdir(img_dir)
                        if os.path.isfile(os.path.join(img_dir, x)) and x != '.DS_Store'])
             lst_path.append(os.path.join(img_dir,img))
-
-        image = img_resize_open(lst_path[0], lst, lst_path)
+        #random text size 
+        standard_height = random.choice(random_text_size)
+        image = img_resize_open(lst_path[0], lst, lst_path, standard_height)
         for i in range(1,len(lst)):
             if lst[i] in operation_lst+symbol:
-                image = helper.append_images([image, img_resize_open(lst_path[i], lst, lst_path)], direction='horizontal', aligment='center',
-                                bg_color=(255, 255, 255))
+                image = helper.append_images([image, img_resize_open(lst_path[i], lst, lst_path, standard_height)], 
+                                              direction='horizontal', aligment='center', 
+                                              bg_color=(255, 255, 255))
                 continue
 
-            image = helper.append_images([image, img_resize_open(lst_path[i], lst, lst_path)], direction='horizontal', aligment='bottom',
-                                bg_color=(255, 255, 255))
+            image = helper.append_images([image, img_resize_open(lst_path[i], lst, lst_path, standard_height)],
+                                          direction='horizontal', aligment='bottom',
+                                          bg_color=(255, 255, 255))
         filename = 'Math'+str(batch)+'_' + str(datetime.now())[0:-7].replace('-','').replace(' ','').replace(':','') + '.png'
 
         old_size = image.size
@@ -92,14 +108,40 @@ def main(args):
         new_im = Image.new("RGB", new_size, color=(255,255,255)) 
         new_im.paste(image, ((new_size[0]-old_size[0])/2,
                           (new_size[1]-old_size[1])/2))
-
-        new_im.save(os.path.join(output_path,filename))
-
-        #output latex as .txt
+        
+        open_cv_image = np.array(new_im) 
+        
+        if backgrounded:
+            background_images = []
+            for file in glob.glob(os.path.join(background_path,"*.png")):
+                background_images.append(str(file))
+            random_background = random.choice(background_images)
+            
+            picked_background = cv2.imread(random_background)
+            height, width, channels = open_cv_image.shape
+            height_bg, width_bg, channels_bg = picked_background.shape
+            #handle when too wide
+            if width_bg < width:
+                print '    too wide, resize ...'
+                open_cv_image = helper.image_resize(open_cv_image, width=width_bg)
+                height, width, channels = open_cv_image.shape
+            crop_bkgd = picked_background[0:height+0, 0:width+0]
+            crop_bkgd = np.float32(crop_bkgd)
+            open_cv_image = (crop_bkgd/255)*open_cv_image
+            
+        cv2.imwrite(os.path.join(output_path,filename), open_cv_image)
+        
+        #new_im.save(os.path.join(output_path,filename))
+        latex_str = ' '.join(lst).replace('times', '\\\\times').replace('div', '\\\\div').replace('neq', '\\\\neq')
+        print '    Generated latex: ', latex_str
         with open(os.path.join(output_path_txt, filename).replace('.png','')+".txt", "w") as text_file:
-            text_file.write(' '.join(lst).replace('times', '\\\\ t i m e s').replace('div', 
-                                                '\\\\ d i v').replace('neq', '\\\\ n e q'))
+            text_file.write(latex_str)
 
+
+
+    
 if __name__ == '__main__':
+    print 'Job started ...'
     main(sys.argv[1:])
+    print 'Job done!'
 
--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from PIL import Image
 import numpy as np
+import cv2
 
 def append_images(images, direction='horizontal',
                   bg_color=(255,255,255), aligment='center'):
@@ -59,3 +60,61 @@ def is_number(s):
         pass
     
     return False
+
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized
+    dim = None
+    (h, w) = image.shape[:2]
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+    resized = cv2.resize(image, dim, interpolation = inter)
+    return resized
+
+def noisy(noise_typ,image):
+    '''
+    returns image with added on noise
+    '''
+    if noise_typ == "gauss":
+        row,col,ch= image.shape
+        mean = 0
+        var = 0.1
+        sigma = var**0.5
+        gauss = np.random.normal(mean,sigma,(row,col,ch))
+        gauss = gauss.reshape(row,col,ch)
+        noisy = image + gauss
+        return noisy
+    elif noise_typ == "s&p":
+        row,col,ch = image.shape
+        s_vs_p = 0.5
+        amount = 0.008
+        out = np.copy(image)
+        # Salt mode
+        num_salt = np.ceil(amount * image.size * s_vs_p)
+        coords = [np.random.randint(0, i - 1, int(num_salt))
+                  for i in image.shape]
+        out[coords] = 1
+
+        # Pepper mode
+        num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+        coords = [np.random.randint(0, i - 1, int(num_pepper))
+                  for i in image.shape]
+        out[coords] = 0
+        return out
+    elif noise_typ == "poisson":
+        vals = len(np.unique(image))
+        vals = 2 ** np.ceil(np.log2(vals))
+        noisy = np.random.poisson(image * vals) / float(vals)
+        return noisy
+    elif noise_typ =="speckle":
+        row,col,ch = image.shape
+        #gauss = np.random.randn(row,col,ch)
+        gauss = np.random.randn(row,col,ch)
+        gauss = gauss.reshape(row,col,ch)         
+        noisy = image + image * gauss
+        return noisy
